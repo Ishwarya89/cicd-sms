e4121cefb506b38ed9cfcb16cdc2aec604c203e5
Ishwarya <ishbodi2017@gmail.com>
2023-11-20 17:54:33 +0530

    sms4

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom' // Import BrowserRouter
 import App from './App'
 
 test('renders learn react link', () => {
-  render(
-    <Router>
-      <App />
-    </Router>,
-  )
-
-  // You might want to use `screen.debug()` here to see the rendered output in the console.
-  // screen.debug();
-
-  // Now, navigate to the /login route to render the Login component
+  render(<App />)
   const linkElement = screen.getByText(/Login Page/i)
   expect(linkElement).toBeInTheDocument()
 })
