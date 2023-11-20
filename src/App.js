dd21fad1759659a3cca1821ae130a61ae9b12f05
Ishwarya <ishbodi2017@gmail.com>
2023-11-20 18:24:14 +0530

    sms7

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <HashRouter>
         <Suspense fallback={loading}>
           <Routes>
-            <h1>Login page</h1>
+            <h1>learn react</h1>
             <Route exact path="/login" name="Login Page" element={<Login />} />
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
-test('renders Login page heading', async () => {
-  await (async () => {
-    render(
-      <Router>
-        <App />
-      </Router>,
-    )
-  })
-
-  // You might want to use `screen.debug()` here to see the rendered output in the console.
-  // screen.debug();
-
-  const headingElement = screen.getByText(/Login page/i)
-  expect(headingElement).toBeInTheDocument()
+test('renders learn react link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
 })
